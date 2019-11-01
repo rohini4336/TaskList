82b30a0ebf3f216768fe88d081db83121ab2647a
Rohini <rohini@cyperts.net>
2019-11-01 16:43:50 +0530

    updated script

--- getaccount.sh
+++ getaccount.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-#echo "Rohini">>l1.txt
-#ls
-#echo "what is your name?"
-#read name
-#echo "How do you do, $name?"
-#read remark
-#echo "I am $remark too!"
+
 echo "enter account name"
 read Aname 
-echo "address=$(./alphaxd getaccountaddress $Aname)">>getaccoount.txt
-#read Aaddress
-#echo "$addr">>getaccount.txt
+
+
+if grep -qF "$Aname=" getaccoount.txt;
+then
+   echo "account already exist"
+else
+   echo "$Aname=$(./alphaxd getaccountaddress $Aname)">>getaccoount.txt
+fi
+
+
+
+echo "enter your withdrawal address"
+read address
+
+echo "balance of $address =$(./alphaxd  getbalance $address)">>checkbalance.txt
+balance="$(./alphaxd  getbalance $address)"
+
+echo "bal=$balance"
+Fee=0.45
+
+
+if [ `echo "$balance>$Fee"|bc` -eq 1 ]; 
+then
+       echo "you can transfer amount=$(($balance-0.45))"
+else
+       echo "Not Sufficient Amout  to Transfer"
+
+
+fi
 
 
 
