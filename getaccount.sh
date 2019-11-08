d6760cf36fdd87e28fd4764eadd257532d3004ee
Rohini <rohini@cyperts.net>
2019-11-08 16:51:47 +0530

    updated script

--- getaccount.sh
+++ getaccount.sh
@@ -11,36 +11,60 @@ else
    echo "$Aname=$(./CypertsCoind getaccountaddress $Aname)">>getaccoount.txt
 fi
 
+ yes="y"
+  no="n"
 
+if grep -qF "$Aname=" getaccoount.txt;
+then
+   echo "do you want to check your balance?y/n"
+   read response
+ 
+if [ "$response" == "$yes" ];
+then
+echo "enter your account"
+read account
 
-#echo "enter your withdrawal address"
-#read address
-
-echo "balance of $address =$(./CypertsCoind  getbalance)">>checkbalance.txt
-balance="$(./CypertsCoind  getbalance $address)"
+echo "balance of $account =$(./CypertsCoind  getbalance $account)">>checkbalance.txt
+balance="$(./CypertsCoind  getbalance $account)"
 
 echo "bal=$balance"
-Fee=0.45
-
-
-if [ `echo "$balance>$Fee"|bc` -eq 1 ]; 
+ Fee=0.45
+elif [ "$response" == "$no" ];
 then
-       #echo "you can transfer amount=((`expr $balance- $Fee` ))"
-val=`echo expr $balance - $Fee`
-echo "balance - Fee : $val"
+echo   "ok"
+else
+echo "sorry wrong choice....y/n"
+fi 
+fi 
+
+
+
+echo "do you want to transfer ur balance ?y/n"
+   read resp
+if [ "$resp" == "$yes" ];
+then
+
+ if [ `echo "$balance>$Fee"|bc` -eq 1 ]; 
+then
+       echo "transferable amount=((`expr $balance` ))"
+#val=`echo expr $balance - $Fee`
+#echo "balance - Fee : $val"
 else
        echo "Not Sufficient Amout  to Transfer"
-
-
+#elif [ "$resp" == "$no" ]
+#echo "ok"
+#else
+#echo "wrong choice"
+fi
 fi
 
+ echo "enter your address to transfer"
+  read addr
+ echo "enter amount"
+  read amount
 
-
-
-
-
-
-
-
-
-
+if [ `echo "$balance>$Fee"|bc` -eq 1 ]; 
+then 
+echo "$(./CypertsCoind sendfrom ""$account"" ""$addr"" "$amount")"
+    echo "done"
+fi
